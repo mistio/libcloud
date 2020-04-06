ac55b7e1313d94e9041e8a8549e77d0e9795288b
Eis <monoatomic.alex@gmail.com>
2020-04-06 19:30:04 +0300

    Changed price getting for sizes to be the same as azure, changed scrape prices url for current gen instance types, changed aws price list in pricing.json to be the same as azure with ec2_linux and ec2_azure --> instance type --> region --> price

--- contrib/scrape-ec2-prices.py
+++ contrib/scrape-ec2-prices.py
@@ -24,6 +24,7 @@ import time
 from collections import defaultdict, OrderedDict
 
 import requests
+import demjson  # pylint: disable=import-error
 
 LINUX_PRICING_URLS = [
     # Deprecated instances (JSON format)
@@ -31,7 +32,8 @@ LINUX_PRICING_URLS = [
     # Previous generation instances (JavaScript file)
     'https://a0.awsstatic.com/pricing/1/ec2/previous-generation/linux-od.min.js',
     # New generation instances (JavaScript file)
-    'https://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js'
+    # Using other endpoint atm
+    # 'https://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js'
 ]
 
 EC2_REGIONS = [
@@ -42,18 +44,15 @@ EC2_REGIONS = [
     'us-gov-west-1',
     'eu-west-1',
     'eu-west-2',
-    'eu-west-3',
     'eu-central-1',
     'ca-central-1',
     'ap-southeast-1',
     'ap-southeast-2',
     'ap-northeast-1',
     'ap-northeast-2',
-    'ap-northeast-3',
     'ap-south-1',
     'sa-east-1',
     'cn-north-1',
-    'me-south-1'
 ]
 
 EC2_INSTANCE_TYPES = [
@@ -161,23 +160,69 @@ PRICING_FILE_PATH = os.path.abspath(PRICING_FILE_PATH)
 
 
 def scrape_ec2_pricing():
-    #https://calculator.aws/pricing/1.0/ec2/region/us-east-1/ondemand/windows-std/index.json
     result = defaultdict(OrderedDict)
     os_map = {'linux': 'ec2_linux', 'windows-std': 'ec2_windows'}
-    url = 'https://calculator.aws/pricing/1.0/ec2/region/{}/ondemand/{}/index.json'
+    for item in os_map.values():
+        result[item] = {}
+    for url in LINUX_PRICING_URLS:
+        response = requests.get(url)
+
+        if re.match(r'.*?\.json$', url):
+            data = response.json()
+        elif re.match(r'.*?\.js$', url):
+            data = response.content.decode()
+            match = re.match(r'^.*callback\((.*?)\);?$', data,
+                             re.MULTILINE | re.DOTALL)
+            data = match.group(1)
+            # demjson supports non-strict mode and can parse unquoted objects
+            data = demjson.decode(data)
+        regions = data['config']['regions']
+
+        for region_data in regions:
+            region_name = region_data['region']
+            instance_types = region_data['instanceTypes']
+
+            for instance_type in instance_types:
+                sizes = instance_type['sizes']
+                for size in sizes:
+                    if not result['ec2_linux'].get(size['size'], False):
+                        result['ec2_linux'][size['size']] = {}
+                    price = size['valueColumns'][0]['prices']['USD']
+                    if str(price).lower() == 'n/a':
+                        # Price not available
+                        continue
+
+                    result['ec2_linux'][size['size']][
+                        region_name] = float(price)
+
+    res = defaultdict(OrderedDict)
+    url = ('https://calculator.aws/pricing/1.0/'
+           'ec2/region/{}/ondemand/{}/index.json')
+    instances = set()
     for OS in ['linux', 'windows-std']:
-        result[os_map[OS]] = {}
+        res[os_map[OS]] = {}
         for region in EC2_REGIONS:
-            result[os_map[OS]][region]={}
+            res[os_map[OS]][region] = {}
             response = requests.get(url.format(region, OS))
             if response.status_code != 200:
                 continue
             data = response.json()
-            
+
             for entry in data['prices']:
-                instance_type = entry['attributes'].get('aws:ec2:instanceType',"")
+                instance_type = entry['attributes'].get(
+                    'aws:ec2:instanceType', "")
+                instances.add(instance_type)
                 price = entry['price'].get('USD', 0)
-                result[os_map[OS]][region][instance_type] = price
+                res[os_map[OS]][region][instance_type] = price
+    for item in os_map.values():
+        for instance in instances:
+            if not result[item].get(instance, False):
+                result[item][instance] = {}
+            for region in EC2_REGIONS:
+                if res[item][region].get(instance, False):
+                    result[item][instance][region] = float(res[
+                        item][region][instance])
+
     return result
 
 
@@ -190,7 +235,7 @@ def update_pricing_file(pricing_file_path, pricing_data):
     data['compute'].update(pricing_data)
 
     # Always sort the pricing info
-    #data = sort_nested_dict(data)
+    data = sort_nested_dict(data)
 
     content = json.dumps(data, indent=4)
     lines = content.splitlines()
